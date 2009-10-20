4926600440f372909129b2ebc818284c6bbed82e
Leandro Silva <leandrodoze@gmail.com>
2009-10-19 22:08:07 -0700

    complete refactoring to add 'configure' task and './otp_kickoff' configuration file

--- lib/otp_kickoff.rb
+++ lib/otp_kickoff.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
 require 'thor'
+require 'yaml'
 
-class OTPKickOff < Thor::Group
+#
+# Path to configuration file
+#
+CONFIGURATION_FILE = '~/.otp_kickoff'
+
+#
+# Thor classes, the magic of tasks!
+#
+class OTPKickOff < Thor
   include Thor::Actions
-  
-  argument :application_name
-  argument :author_name
-  argument :author_email
   
   #
   # Setup
@@ -17,25 +22,49 @@ class OTPKickOff < Thor::Group
   end
   
   def self.destination_root
-    File.expand_path(File.dirname(OTPKickOff.source_root))
-  end
-  
-  def today
-    Date.today
+    File.expand_path(File.dirname('.'))
   end
   
   #
-  # Steps
+  # Tasks
   #
+
+  no_tasks { attr_accessor :application_name, :author_name, :author_email, :today }
   
-  def create_application_directories
-    empty_directory "#{application_name}/src"
-    empty_directory "#{application_name}/ebin"
-    empty_directory "#{application_name}/deps"
-    empty_directory "#{application_name}/priv"
+  # task: configure
+  
+  desc 'configure --author=AUTHOR --email=AUTHOR_EMAIL', 'configure informations about author'
+  method_options :author => :string, :email => :string
+  
+  def configure
+    #set data about author
+    @author_name  = options[:author]
+    @author_email = options[:email]
+    
+    create_file CONFIGURATION_FILE do
+      %Q{
+author_name: #{author_name}
+author_email: #{author_email}
+      }
+    end
   end
   
-  def create_source_files
+  # task: new_application
+  
+  desc 'new_application --name=APPLICATION_NAME', 'generate a new Erlang/OTP application stub'
+  method_options :name => :string
+  
+  def new_application
+    # set data to templates
+    @application_name = options[:name]
+    
+    config_info   = ConfigInfo.new
+    @author_name  = config_info.author_name
+    @author_email = config_info.author_email
+    
+    @today = Date.today
+    
+    # generate templates
     template 'resources/template_app.app',      "#{application_name}/src/#{application_name}_app.app"
     template 'resources/template_app.erl',      "#{application_name}/src/#{application_name}_app.erl"
     template 'resources/template_sup.erl',      "#{application_name}/src/#{application_name}_sup.erl"
@@ -43,5 +72,32 @@ class OTPKickOff < Thor::Group
     template 'resources/template_handler.erl',  "#{application_name}/src/#{application_name}_handler.erl"
     template 'resources/template_appstart.erl', "#{application_name}/src/#{application_name}.erl"
     template 'resources/template_include.hrl',  "#{application_name}/src/#{application_name}.hrl"
+
+    # create required directories
+    empty_directory "#{application_name}/ebin"
+    empty_directory "#{application_name}/deps"
+    empty_directory "#{application_name}/priv"
+  end
+end
+
+#
+# Helper class to get informations from configuration file (~/.otp_kickoff)
+#
+class ConfigInfo
+  attr_accessor :author_name, :author_email
+  
+  def initialize
+    begin
+      info = YAML.load_file(File.expand_path(CONFIGURATION_FILE))
+    rescue
+      puts "I'm sorry but the configuration file doesn't exists."
+      puts "Please run the command below:"
+      puts "  otp_kickoff configure --author=YOUR_NAME --email=YOUR_EMAIL"
+      
+      exit 0
+    end
+    
+    @author_name  = info["author_name"]
+    @author_email = info["author_email"]
   end
 end
