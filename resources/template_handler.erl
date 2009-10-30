f5272abc09b7bc374e9e634a9ad8a02dd6b220b9
Leandro Silva <leandrodoze@gmail.com>
2009-10-29 22:51:02 -0700

    change parameters and commets

--- resources/template_handler.erl
+++ resources/template_handler.erl
@@ -1,7 +1,7 @@
 %%
-%% Handler module that can be used by server to catch and handle events
+%% Handler module used by server (specially on event manager) to deal with events
 %%
-%% File   : <%=application_name%>_handler.erl
+%% File   : <%=handler_name%>_handler.erl
 %% Created: <%=Date.today%>
 %%
 %% @author <%=author_name%> <<%=author_email%>>
@@ -10,26 +10,26 @@
 %% @doc TODO make nice description
 %%
 
--module(<%=application_name%>_handler).
+-module(<%=handler_name%>_handler).
 -author('<%=author_name%> <<%=author_email%>>').
 
 -export([init/1, terminate/1, handle_event/2]).
 
 %% @spec init(State) -> State
-%% @doc Handler init callback
+%% @doc Handler's init callback
 init(State) -> State.
 
 %% @spec terminate(State) -> State
-%% @doc Handler terminate callback
+%% @doc Handler's terminate callback
 terminate(State) ->
   {ok, State}.
 
 %% @spec handle_event({event, _Event}, State) -> {ok, State}
-%% @doc Handle event function
+%% @doc Handle's event callback
 handle_event({event, _Event}, State) ->
   {ok, State};
 
 %% @spec handle_event(_Event, State) -> State
-%% @doc Handle event "catch" function
+%% @doc Handler's "catch all event" callback
 handle_event(_Event, State) ->
   State.
