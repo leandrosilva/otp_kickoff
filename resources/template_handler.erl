6bb8f62ecbb977b372aa9a0bd9c9833995432a3f
Leandro Silva <leandrodoze@gmail.com>
2009-10-31 10:12:38 -0700

    some fix on template_handler

--- resources/template_handler.erl
+++ resources/template_handler.erl
@@ -1,5 +1,5 @@
 %%
-%% Handler module used by server (specially on event manager) to deal with events
+%% Handler module used by event manager to deal with events
 %%
 %% File   : <%=handler_name%>_handler.erl
 %% Created: <%=Date.today%>
@@ -13,23 +13,36 @@
 -module(<%=handler_name%>_handler).
 -author('<%=author_name%> <<%=author_email%>>').
 
--export([init/1, terminate/1, handle_event/2]).
+-behahiour(gen_event).
+
+-export([init/1, terminate/1, handle_event/2, handle_call/2, handle_info/2, code_change/3]).
 
 %% @spec init(State) -> State
-%% @doc Handler's init callback
-init(State) -> State.
+%% @doc Callback for initialize the handler
+init(State) ->
+	{ok, State}.
 
-%% @spec terminate(State) -> State
-%% @doc Handler's terminate callback
+%% @spec terminate(State) -> {ok, State}
+%% @doc Callback for cleanup the handler
 terminate(State) ->
   {ok, State}.
 
-%% @spec handle_event({event, _Event}, State) -> {ok, State}
-%% @doc Handle's event callback
-handle_event({event, _Event}, State) ->
-  {ok, State};
-
-%% @spec handle_event(_Event, State) -> State
-%% @doc Handler's "catch all event" callback
+%% @spec handle_event(_Event, State) -> {ok, State}
+%% @doc Callback for notified events on gen_event
 handle_event(_Event, State) ->
-  State.
+  {ok, State}.
+
+%% @spec handle_call(_Request, State) -> {ok, Reply, State}
+%% @doc Callback for specific calls for this handler
+handle_call(_Request, State) ->
+	{ok, noreply, State}.
+
+%% @spec handle_info(_Info, State) -> {ok, State}
+%% @doc Callback for unknow messages by the gen_event
+handle_info(_Info, State) ->
+	{ok, State}.
+
+%% @spec code_change(_OldVsn, State, _Extra) -> {ok, State}
+%% @doc Callback for update internal state during upgrade/downgrade
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
